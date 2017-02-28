da850aac838012520ec755b3829646d5ac44f438
lisn0416 <lisn1234@126.com>
2017-02-28 16:46:06 +1100

    Add files via upload

--- 1D-SWE.py
+++ 1D-SWE.py
@@ -29,14 +29,25 @@ def SWE():
     h = myFunc.addTwoElements(h)
     hu = myFunc.addTwoElements(hu)
 
-    # The wave at point x=0     (amplitude, period, offset, wave, timeArray, length)
-    h_X0 = myFunc.artificialWave(0.01, 12.5, 1, wave, timeArray, timeTotalNumber)
-
+    # The wave at point x=0     (amplitude, period, offset, wave, timeArray, length, ifNoise, noiseRate)
+    h_X0 = myFunc.artificialWave(0.05, 12.5, 1, wave, timeArray, timeTotalNumber, 0, 0.2)
     # myFunc.onePlot(1, timeArray, h_X0, 'Time(s)', 'Amplitude', 'Artificial Wave', 0., timeLength, .9, 1.1)
+
     timeSum = 0
     while timeSum < timeTotalNumber :
         # artificialWave: start boundary condition
         h[1] = h_X0[timeSum]
+
+        if timeSum == numpy.ceil((timeTotalNumber-1)/2) :
+            if 1: # quantisation
+                for i in range(len(h)):
+                    h[i] = round(h[i]*4)/(4)
+                    hu[i] = round(hu[i]*4)/(4)
+            else: # noise
+                for i in range(len(h)):
+                    h[i] += (numpy.random.random() - 0.5) * 0.2
+                    hu[i] += (numpy.random.random() - 0.5) * 0.2
+
         # end boundary condition
         hu[-2] = 0
 
@@ -44,16 +55,23 @@ def SWE():
         h = myFunc.neumannBoundaryConditions(h)
         hu = myFunc.neumannBoundaryConditions(hu)
 
-        u = hu / h
-
         h[1:-1], hu[1:-1] = myFunc.iteration_Lax_Friedrichs(h, hu)
+
+        u = hu / h
         timeSum += 1
 
-        if timeSum == timeTotalNumber :
-            myFunc.onePlot(2, xArray, h[1:-1], 'x(m)', 'h', 'h-x', 0., tunnelLength, .9, 1.1)
-            myFunc.onePlot(3, xArray, u[1:-1], 'x(m)', 'u', 'u-x', 0., tunnelLength, -.3, .3)
+        if timeSum == numpy.ceil((timeTotalNumber-1)) :
+            #myFunc.onePlot(2, xArray, h[1:-1], 'x(m)', 'h(m)', 'Height', 0., tunnelLength, .9, 1.1)
+            #myFunc.onePlot(3, xArray, u[1:-1], 'x(m)', 'u(m/s)', 'Velocity', 0., tunnelLength, -.3, .3)
+            #myFunc.onePlot(4, xArray, hu[1:-1], 'x(m)', 'hu(m^2/s)', 'Momentum', 0., tunnelLength, -.5, .5)
+
+            myFunc.threePlot(5, xArray, h[1:-1], 'x(m)', 'h(m)', 'Height', 0., tunnelLength, .9, 1.1,
+                                xArray, u[1:-1], 'x(m)', 'u(m/s)', 'Velocity', 0., tunnelLength, -.3, .3,
+                                xArray, hu[1:-1], 'x(m)', 'hu(m^2/s)', 'Momentum', 0., tunnelLength, -.5, .5)
+
             pylab.show()
-    print "finish SWE"
+            break
+    print "finish 1D-SWE"
 
 if __name__ == "__main__":
     SWE()
--- myFunc.py
+++ myFunc.py
@@ -7,9 +7,11 @@ timeLength = 1.     # second
 dx = 1. / 10000
 dt = 1. / 60000
 
-def artificialWave(amplitude, period, offset, wave, timeArray, length):
+def artificialWave(amplitude, period, offset, wave, timeArray, length, ifNoise, noiseRate):
     for i in range(length):
         wave[i] = amplitude * numpy.cos(period * 2 * numpy.pi * timeArray[i]) + offset
+        if ifNoise == 1:
+            wave[i] += amplitude * noiseRate * (numpy.random.random() - 0.5 * offset)
     return wave
 
 
@@ -49,7 +51,55 @@ def onePlot(figNum, x, y, xLabel, yLabel, title, xLimL, xLimR, yLimL, yLimR):
     pylab.axis('tight')
     pylab.xlim(xLimL, xLimR)
     pylab.ylim(yLimL, yLimR)
+    pylab.grid()
     pylab.draw()
+
+
+def threePlot(figNum, x1, y1, xLabel1, yLabel1, title1, xLimL1, xLimR1, yLimL1, yLimR1,
+                      x2, y2, xLabel2, yLabel2, title2, xLimL2, xLimR2, yLimL2, yLimR2,
+                      x3, y3, xLabel3, yLabel3, title3, xLimL3, xLimR3, yLimL3, yLimR3):
+    # Figure(figsize=None, dpi=None, facecolor=None, edgecolor=None, linewidth=0.0, frameon=None, subplotpars=None, tight_layout=None)
+    pylab.figure(figNum, figsize=(8, 3), dpi=300)
+    pylab.subplots_adjust(left=0.08, right=0.97, bottom=0.15, top=0.9, wspace=0.3)
+
+    pylab.subplot(131)
+    pylab.title(title1)
+    pylab.plot(x1, y1, linewidth=1.0)
+    pylab.axis('tight')
+    pylab.xlabel(xLabel1, fontsize=6)
+    pylab.ylabel(yLabel1, fontsize=6)
+    pylab.xlim(xLimL1, xLimR1)
+    pylab.ylim(yLimL1, yLimR1)
+    pylab.xticks(fontsize=6)
+    pylab.yticks(fontsize=6)
+    pylab.grid()
+
+    pylab.subplot(132)
+    pylab.title(title2)
+    pylab.plot(x2, y2, linewidth=1.0)
+    pylab.axis('tight')
+    pylab.xlabel(xLabel2, fontsize=6)
+    pylab.ylabel(yLabel2, fontsize=6)
+    pylab.xlim(xLimL2, xLimR2)
+    pylab.ylim(yLimL2, yLimR2)
+    pylab.xticks(fontsize=6)
+    pylab.yticks(fontsize=6)
+    pylab.grid()
+
+    pylab.subplot(133)
+    pylab.title(title3)
+    pylab.plot(x3, y3, linewidth=1.0)
+    pylab.axis('tight')
+    pylab.xlabel(xLabel3, fontsize=6)
+    pylab.ylabel(yLabel3, fontsize=6)
+    pylab.xlim(xLimL3, xLimR3)
+    pylab.ylim(yLimL3, yLimR3)
+    pylab.xticks(fontsize=6)
+    pylab.yticks(fontsize=6)
+    pylab.grid()
+
+    pylab.draw()
+    pylab.savefig(title1+'+'+title2+'+'+title3, facecolor='w', edgecolor='w')
 
 
 if __name__ == "__main__":
